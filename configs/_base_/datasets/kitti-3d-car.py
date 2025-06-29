49cadad66f40bc100eedc3addb88498674250aba
Daham Positha Pathiraja <dahampositha33@gmail.com>
2025-06-29 15:38:30 +0530

    Update kitti-3d-car.py

--- configs/_base_/datasets/kitti-3d-car.py
+++ configs/_base_/datasets/kitti-3d-car.py
@@ -100,7 +100,7 @@ eval_pipeline = [
 ]
 train_dataloader = dict(
     batch_size=6,
-    num_workers=4,
+    num_workers=2,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
