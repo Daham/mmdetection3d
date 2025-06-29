9dc7ed89534d2e048b74367c45a5c679bc6c2dba
Daham Positha Pathiraja <dahampositha33@gmail.com>
2025-06-29 12:01:50 +0530

    Update single_stage.py

--- mmdet3d/models/detectors/single_stage.py
+++ mmdet3d/models/detectors/single_stage.py
@@ -106,7 +106,7 @@ class SingleStage3DDetector(Base3DDetector):
                 - bboxes_3d (Tensor): Contains a tensor with shape
                     (num_instances, C) where C >=7.
         """
-        x = self.extract_feat(batch_inputs_dict)
+        x = self.extract_feat(batch_inputs_dict, batch_data_samples)
         results_list = self.bbox_head.predict(x, batch_data_samples, **kwargs)
         predictions = self.add_pred_to_datasample(batch_data_samples,
                                                   results_list)
