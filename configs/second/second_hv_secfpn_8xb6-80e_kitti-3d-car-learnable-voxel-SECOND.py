658fa5f5fe97aada16af03d7b744a803aa3b1b04
Daham <dahampositha33@gmail.com>
2025-07-27 19:44:47 +0530

    Change base config

--- configs/second/second_hv_secfpn_8xb6-80e_kitti-3d-car-learnable-voxel-SECOND.py
+++ configs/second/second_hv_secfpn_8xb6-80e_kitti-3d-car-learnable-voxel-SECOND.py
@@ -4,123 +4,56 @@ _base_ = [
     '../_base_/default_runtime.py'
 ]
 
-# Remove the base model config and define everything ourselves
 voxel_size = [0.5, 0.5, 0.5]
 point_cloud_range = [0, -40, -3, 70.4, 40, 1]
 data_root = '/home/daham/mmdetection_project/dataset/KITTI/'
 
-# Define the complete model configuration
+# Use the simplest possible model configuration
 model = dict(
     type='VoxelNet',
-    data_preprocessor=dict(
-        type='Det3DDataPreprocessor',
-        voxel=True,
-        voxel_layer=dict(
-            max_num_points=100,
-            point_cloud_range=point_cloud_range,
-            voxel_size=voxel_size,
-            max_voxels=(16000, 40000))),
-    pts_voxel_encoder=dict(
+    voxel_layer=dict(
+        max_num_points=100,
+        point_cloud_range=point_cloud_range,
+        voxel_size=voxel_size,
+        max_voxels=(16000, 40000)),
+    voxel_encoder=dict(
         type='LearnableVFE',
         in_channels=4,
         feat_channels=[64],
         with_distance=False,
         voxel_size=voxel_size,
         point_cloud_range=point_cloud_range),
-    pts_middle_encoder=dict(
+    middle_encoder=dict(
         type='SparseEncoder',
         in_channels=64,
-        sparse_shape=[41, 1600, 1408],
-        order=('conv', 'norm', 'act')),
-    pts_backbone=dict(
+        sparse_shape=[41, 1600, 1408]),
+    backbone=dict(
         type='SECOND',
         in_channels=64,
         out_channels=[64, 128],
         layer_nums=[5, 5],
-        layer_strides=[1, 2],
-        norm_cfg=dict(type='BN', eps=1e-3, momentum=0.01),
-        conv_cfg=dict(type='Conv2d', bias=False)),
-    pts_neck=dict(
+        layer_strides=[1, 2]),
+    neck=dict(
         type='SECONDFPN',
         in_channels=[64, 128],
         out_channels=[128, 128],
-        upsample_strides=[1, 2],
-        norm_cfg=dict(type='BN', eps=1e-3, momentum=0.01),
-        upsample_cfg=dict(type='deconv', bias=False),
-        use_conv_for_no_stride=True),
-    pts_bbox_head=dict(
+        upsample_strides=[1, 2]),
+    bbox_head=dict(
         type='Anchor3DHead',
         num_classes=1,
         in_channels=256,
         feat_channels=256,
-        use_direction_classifier=True,
         anchor_generator=dict(
             type='Anchor3DRangeGenerator',
             ranges=[[0, -40.0, -3.0, 70.4, 40.0, 1.0]],
             sizes=[[3.9, 1.6, 1.56]],
-            rotations=[0, 1.57],
-            reshape_out=True),
-        diff_rad_by_sin=True,
+            rotations=[0, 1.57]),
         bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder'),
-        loss_cls=dict(
-            type='FocalLoss',
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=1.0),
-        loss_bbox=dict(
-            type='SmoothL1Loss',
-            beta=1.0 / 9.0,
-            loss_weight=2.0),
-        loss_dir=dict(
-            type='CrossEntropyLoss',
-            use_sigmoid=False,
-            loss_weight=0.2)),
-    train_cfg=dict(
-        pts=dict(
-            assigner=dict(
-                type='Max3DIoUAssigner',
-                iou_calculator=dict(type='BboxOverlapsNearest3D'),
-                pos_iou_thr=0.6,
-                neg_iou_thr=0.45,
-                min_pos_iou=0.45,
-                ignore_iof_thr=-1),
-            allowed_border=0,
-            pos_weight=-1,
-            debug=False)),
-    test_cfg=dict(
-        pts=dict(
-            use_rotate_nms=True,
-            nms_across_levels=False,
-            nms_thr=0.01,
-            score_thr=0.1,
-            min_bbox_size=0,
-            nms_pre=100,
-            max_num=50)))
+        loss_cls=dict(type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0),
+        loss_dir=dict(type='CrossEntropyLoss', use_sigmoid=False)))
 
-# Training configuration
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=5, val_interval=1)
-val_cfg = dict()
-test_cfg = dict()
-
-# Optimizer
+# Keep your original training settings
 optim_wrapper = dict(
-    type='OptimWrapper',
-    optimizer=dict(type='AdamW', lr=0.0002, weight_decay=0.01))
-
-# Learning rate scheduler
-param_scheduler = [
-    dict(
-        type='LinearLR',
-        start_factor=1.0 / 1000,
-        by_epoch=False,
-        begin=0,
-        end=1000),
-    dict(
-        type='CosineAnnealingLR',
-        T_max=5,
-        eta_min=0.0002 * 0.05,
-        begin=0,
-        end=5,
-        by_epoch=True)
-]
+    optimizer=dict(type='AdamW', lr=0.0002, weight_decay=0.01)
+)
