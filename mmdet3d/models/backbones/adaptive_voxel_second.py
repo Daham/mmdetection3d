34ccd1b5ed9dd1f5d8e90a056f168505fabb3784
Daham <dahampositha33@gmail.com>
2025-07-27 19:18:18 +0530

    Change AdaptiveVoxelSECOND

--- mmdet3d/models/backbones/adaptive_voxel_second.py
+++ mmdet3d/models/backbones/adaptive_voxel_second.py
@@ -1,8 +1,11 @@
-from mmcv.runner import auto_fp16
-from mmdet3d.models import DETECTORS, build_backbone, build_neck, build_head
-from mmdet3d.models.voxel_encoders.learnable_vfe import LearnableVFE
+import torch
+from torch import nn
+from mmengine.runner import auto_fp16  # Changed from mmcv.runner
+from mmdet3d.registry import MODELS
+from mmdet3d.models import build_backbone, build_neck, build_head
+from mmdet3d.models.builder import build_voxel_encoder
 
-@DETECTORS.register_module()
+@MODELS.register_module()
 class AdaptiveVoxelSECOND(nn.Module):
     """
     SECOND-based 3D detector with per-location learnable voxel support size.
@@ -33,26 +36,6 @@ class AdaptiveVoxelSECOND(nn.Module):
     @auto_fp16(apply_to=('points', ))
     def extract_feat(self, points, img_metas=None):
         voxels, coords, num_points = self.voxel_encoder(points)
-        x = self.backbone(voxels, coords, num_points)
-        if self.neck:
-            x = self.neck(x)
-        return x
+        # Continue with the rest of your implementation...
+        pass
 
-    def forward_train(self,
-                      points,
-                      img_metas,
-                      gt_bboxes_3d,
-                      gt_labels_3d,
-                      gt_bboxes_ignore=None):
-        x = self.extract_feat(points, img_metas)
-        losses = self.bbox_head.forward_train(
-            x, img_metas, gt_bboxes_3d, gt_labels_3d,
-            gt_bboxes_ignore=gt_bboxes_ignore)
-        return losses
-
-    def simple_test(self, points, img_metas, rescale=False):
-        x = self.extract_feat(points, img_metas)
-        return self.bbox_head.simple_test(x, img_metas, rescale=rescale)
-
-    def aug_test(self, points, img_metas, rescale=False):
-        return self.simple_test(points, img_metas, rescale)
