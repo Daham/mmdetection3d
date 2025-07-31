ea612fd894a7e32f975e3ee491b209e795d289db
Daham <dahampositha33@gmail.com>
2025-07-31 11:44:15 +0530

    Change config

--- configs/second/second_hv_secfpn_8xb6-80e_kitti-3d-car-SECOND.py
+++ configs/second/second_hv_secfpn_8xb6-80e_kitti-3d-car-SECOND.py
@@ -1,34 +1,38 @@
-_base_ = './second_hv_secfpn_8xb6-80e_kitti-3d-car-SECOND.py'
+_base_ = [
+    '../_base_/models/second_hv_secfpn_kitti.py',
+    '../_base_/datasets/kitti-3d-car.py', '../_base_/schedules/cyclic-2e.py',
+    '../_base_/default_runtime.py'
+]
 
-# Import FocalLoss from mmdet if not already registered
-custom_imports = dict(
-    imports=['mmdet.models.losses.focal_loss'],  # or use your custom path if different
-    allow_failed_imports=False
-)
 
-# Only override the voxel encoder to use LearnableVFE
+voxel_size = [0.5, 0.5, 0.5]
+point_cloud_range = [0, -40, -3, 70.4, 40, 1]
+data_root = '/home/daham/mmdetection_project/dataset/KITTI/' 
+
 model = dict(
-    voxel_encoder=dict(
-        type='LearnableVFE',
-        in_channels=4,
-        feat_channels=[64],
-        with_distance=False,
-        voxel_size=[0.5, 0.5, 0.5],
-        point_cloud_range=[0, -40, -3, 70.4, 40, 1]
-    ),
     bbox_head=dict(
-        loss_cls=dict(
-            type='FocalLoss',  # Make sure FocalLoss is imported and registered
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=1.0
-        )
-        # Keep other loss_* and bbox_coder settings from base config unless you want to override
-    )
-)
+        num_classes=1,
+        anchor_generator=dict(
+            _delete_=True,
+            type='Anchor3DRangeGenerator',
+            ranges=[[0, -40.0, -3.0, 70.4, 40.0, 1.0]],
+            sizes=[[3.9, 1.6, 1.56]],
+            rotations=[0, 1.57],
+            reshape_out=True)),
+    train_cfg=dict(
+        _delete_=True,
+        max_epochs=5,
+        assigner=dict(
+            type='Max3DIoUAssigner',
+            iou_calculator=dict(type='BboxOverlapsNearest3D'),
+            pos_iou_thr=0.6,
+            neg_iou_thr=0.45,
+            min_pos_iou=0.45,
+            ignore_iof_thr=-1),
+        allowed_border=0,
+        pos_weight=-1,
+        debug=False))
 
-# Keep your optimizer
 optim_wrapper = dict(
     optimizer=dict(type='AdamW', lr=0.0002, weight_decay=0.01)
 )
