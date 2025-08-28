50ee9776a485f8e20941e6b99eddb1ef233ab71c
Daham <dahampositha33@gmail.com>
2025-08-28 07:24:53 +0530

    Cleanup the branch

--- configs/second/second_hv_secfpn_8xb6-80e_kitti-3d-car.py
+++ configs/second/second_hv_secfpn_8xb6-80e_kitti-3d-car.py
@@ -1,34 +1,23 @@
 _base_ = [
     '../_base_/models/second_hv_secfpn_kitti.py',
-    '../_base_/datasets/kitti-3d-car.py', '../_base_/schedules/cyclic-2e.py',
+    '../_base_/datasets/kitti-3d-car.py', '../_base_/schedules/cyclic-40e.py',
     '../_base_/default_runtime.py'
 ]
-
-
-voxel_size = [0.5, 0.5, 0.5]
 point_cloud_range = [0, -40, -3, 70.4, 40, 1]
-data_root = '/home/daham/mmdetection_project/dataset/KITTI/' 
-
 model = dict(
-    voxel_encoder=dict(
-        type='AdaptiveVFE',                          # picks up the class above
-        base_vfe_cfg=dict(
-            type='HardSimpleVFE',
-            num_features=4
-        )
-    ),
     bbox_head=dict(
+        type='Anchor3DHead',
         num_classes=1,
         anchor_generator=dict(
             _delete_=True,
             type='Anchor3DRangeGenerator',
-            ranges=[[0, -40.0, -3.0, 70.4, 40.0, 1.0]],
+            ranges=[[0, -40.0, -1.78, 70.4, 40.0, -1.78]],
             sizes=[[3.9, 1.6, 1.56]],
             rotations=[0, 1.57],
             reshape_out=True)),
+    # model training and testing settings
     train_cfg=dict(
         _delete_=True,
-        max_epochs=5,
         assigner=dict(
             type='Max3DIoUAssigner',
             iou_calculator=dict(type='BboxOverlapsNearest3D'),
@@ -39,7 +28,3 @@ model = dict(
         allowed_border=0,
         pos_weight=-1,
         debug=False))
-
-optim_wrapper = dict(
-    optimizer=dict(type='AdamW', lr=0.0002, weight_decay=0.01)
-)
--- mmdet3d/models/middle_encoders/__init__.py
+++ mmdet3d/models/middle_encoders/__init__.py
@@ -3,20 +3,8 @@ from .pillar_scatter import PointPillarsScatter
 from .sparse_encoder import SparseEncoder, SparseEncoderSASSD
 from .sparse_unet import SparseUNet
 from .voxel_set_abstraction import VoxelSetAbstraction
-from .multi_scale_parallel_middle_encoder import MultiScaleParallelMiddleEncoder
-# 🚀 Optimized adaptive middle encoder (temporarily disabled due to spconv issues)
-# from .optimized_multi_scale_parallel_middle_encoder import OptimizedMultiScaleParallelMiddleEncoder
-from .fallback_multi_scale_parallel_middle_encoder import FallbackMultiScaleParallelMiddleEncoder
-from .efficient_multi_scale_parallel_middle_encoder import EfficientMultiScaleParallelMiddleEncoder
-from .enhanced_multi_scale_parallel_middle_encoder import EnhancedMultiScaleParallelMiddleEncoder
-from .cpu_compatible_encoder import CPUCompatibleDenseEncoder
 
 __all__ = [
-    'PointPillarsScatter', 'SparseEncoder', 'SparseEncoderSASSD',
-    'SparseUNet', 'VoxelSetAbstraction', 'MultiScaleParallelMiddleEncoder',
-    # 'OptimizedMultiScaleParallelMiddleEncoder'  # Temporarily disabled
-    'FallbackMultiScaleParallelMiddleEncoder',  # Spconv-free fallback solution
-    'EfficientMultiScaleParallelMiddleEncoder',  # High-performance vectorized solution
-    'EnhancedMultiScaleParallelMiddleEncoder',  # For advanced multi-scale VFE with attention
-    'CPUCompatibleDenseEncoder',  # CPU-compatible dense encoder for testing adaptive voxelization
+    'PointPillarsScatter', 'SparseEncoder', 'SparseEncoderSASSD', 'SparseUNet',
+    'VoxelSetAbstraction'
 ]
